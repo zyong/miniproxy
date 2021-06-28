19710d456d895a4dd600faf0dcb6cd91f035142c
mark <zhaoyong-lc@gmail.com>
2021-06-28 21:12:43 +0800

    fix args bug

--- miniproxy.sh
+++ miniproxy.sh
@@ -1,9 +1,1 @@
-mvn exec:java@inprocess -Dmain.args="$*" $JAVA_OPTS
-if [ $? != 0 ]; then
-    exit $?;
-fi
-if [ -f out/artifacts/miniproxy_jar/miniproxy.jar ]
-then
-  java -jar out/artifacts/miniproxy_jar/miniproxy.jar $*
-
-fi
+mvn exec:java@inprocess -Dexec.args="$*" $JAVA_OPTS
